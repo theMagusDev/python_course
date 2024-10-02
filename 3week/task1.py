a8e7ab7f0c1e968dd7f813628bb0d6302eff1d5f
Yuriy Magus <114507764+theMagusDev@users.noreply.github.com>
2024-10-02 10:43:26 +0300

    Solve task1 week3

---
+++ 3week/task1.py
@@ -0,0 +1,25 @@
+"""
+https://leetcode.com/problem-list/array/
+https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/
+"""
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) == 1 or len(nums) == 0:
+            return len(nums)
+        left_ptr, right_ptr = 0, 0
+        while right_ptr < len(nums) - 1:
+            right_ptr += 1
+            if nums[left_ptr] != nums[right_ptr]:
+                if (right_ptr - 1) - left_ptr >= 2:
+                    del nums[left_ptr + 1:right_ptr - 1]
+                    left_ptr += 2
+                else:
+                    left_ptr = right_ptr
+                right_ptr = left_ptr + 1
+            elif right_ptr == len(nums) - 1:
+                if (right_ptr - 1) - (left_ptr + 1) + 1 >= 1:
+                    del nums[left_ptr + 1:right_ptr]
+        return len(nums)
