996a33deb7549135b3aac450aa8a4c1668787f1a
Yuriy Magus <114507764+theMagusDev@users.noreply.github.com>
2024-10-28 19:28:33 +0300

    Add task3 week6

---
+++ 6week/task3.py
@@ -0,0 +1,20 @@
+"""
+https://leetcode.com/problem-list/string/
+https://leetcode.com/problems/group-anagrams/
+"""
+
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sorted_strs = strs.copy()
+        for i in range(len(sorted_strs)):
+            sorted_strs[i] = ''.join(sorted([c for c in strs[i]]))
+        groups = dict()
+        for i in range(len(sorted_strs)):
+            if sorted_strs[i] not in groups:
+                groups.update({sorted_strs[i]: [strs[i]]})
+            else:
+                groups[sorted_strs[i]].append(strs[i])
+        return [elements for elements in groups.values()]
