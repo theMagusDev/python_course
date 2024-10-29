c143ac95e517528d91770917b49f7707edb954f5
Yuriy Magus <114507764+theMagusDev@users.noreply.github.com>
2024-10-29 09:37:47 +0300

    fix codestyle in tesk5 6week

--- 6week/task5.py
+++ 6week/task5.py
@@ -1,3 +1,9 @@
+"""
+https://leetcode.com/problem-list/string/
+https://leetcode.com/problems/longest-palindromic-substring/
+"""
+
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         longest_palindrome = s[0]
@@ -5,25 +11,33 @@ class Solution:
         for mid in range(len(s)):
             left = mid
             right = mid
-            while left - 1 >= 0 and right + 1 <= len(s) - 1 and s[left - 1] == s[right + 1]:
+            while (
+                left - 1 >= 0
+                and right + 1 <= len(s) - 1
+                and s[left - 1] == s[right + 1]
+            ):
                 left -= 1
                 right += 1
             current_length = right - left + 1
             if current_length > longest_palindrome_length:
                 longest_palindrome_length = current_length
                 print(left, mid, right)
-                longest_palindrome = s[left:right + 1]
+                longest_palindrome = s[left : right + 1]
         for mid in range(len(s) - 1):
             left = mid
             right = mid + 1
             if s[left] != s[right]:
                 continue
-            while left - 1 >= 0 and right + 1 <= len(s) - 1 and s[left - 1] == s[right + 1]:
+            while (
+                left - 1 >= 0
+                and right + 1 <= len(s) - 1
+                and s[left - 1] == s[right + 1]
+            ):
                 left -= 1
                 right += 1
             current_length = right - left + 1
             if current_length > longest_palindrome_length:
                 longest_palindrome_length = current_length
                 print(left, mid, right)
-                longest_palindrome = s[left:right + 1]
+                longest_palindrome = s[left : right + 1]
         return longest_palindrome
