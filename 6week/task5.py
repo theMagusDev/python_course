701c5dfe4c26705a56248f52aed98cfac2634b9e
Yuriy Magus <114507764+theMagusDev@users.noreply.github.com>
2024-10-29 09:34:24 +0300

    Add tesk5 6week

---
+++ 6week/task5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest_palindrome = s[0]
+        longest_palindrome_length = 1
+        for mid in range(len(s)):
+            left = mid
+            right = mid
+            while left - 1 >= 0 and right + 1 <= len(s) - 1 and s[left - 1] == s[right + 1]:
+                left -= 1
+                right += 1
+            current_length = right - left + 1
+            if current_length > longest_palindrome_length:
+                longest_palindrome_length = current_length
+                print(left, mid, right)
+                longest_palindrome = s[left:right + 1]
+        for mid in range(len(s) - 1):
+            left = mid
+            right = mid + 1
+            if s[left] != s[right]:
+                continue
+            while left - 1 >= 0 and right + 1 <= len(s) - 1 and s[left - 1] == s[right + 1]:
+                left -= 1
+                right += 1
+            current_length = right - left + 1
+            if current_length > longest_palindrome_length:
+                longest_palindrome_length = current_length
+                print(left, mid, right)
+                longest_palindrome = s[left:right + 1]
+        return longest_palindrome
